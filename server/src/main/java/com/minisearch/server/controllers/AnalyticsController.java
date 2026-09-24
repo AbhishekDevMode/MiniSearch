3967cb3a5000f47695acd3911ba03aec86ede57d
AbhishekDevMode <itsabhishek5678@gmail.com>
2026-09-24 23:40:42 +0530

    cors issue fixed

--- server/src/main/java/com/minisearch/server/controllers/AnalyticsController.java
+++ server/src/main/java/com/minisearch/server/controllers/AnalyticsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/analytics")
 @RequiredArgsConstructor
